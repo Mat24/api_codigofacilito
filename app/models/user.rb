6e353536651a83fd08c133d8073a0a0b579af42c
Mat24 <smat_24@hotmail.com>
2015-07-07 18:12:49 -0500

    video: 2-7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
-class User < ActiveRecord::Base
+class  User < ActiveRecord::Base
   has_many :tokens
   validates :email, presence: true, email: true
   validates :provider, presence: true
   validates :uid, presence: true
+
+  def self.from_omniauth(data)
+    User.where(provider: data[:provider], uid: data[:uid], email: data[:info][:email]).first_or_create
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:provider) }
   it { should_not allow_value("lol@lol").for(:email) }
   it { should allow_value("lol@lol.com.es").for(:email) }
+
+  it "deberia crear un usuario si el uid y el provider no existen" do
+    expect{
+      User.from_omniauth({uid:"12345", provider: "facebook", info: {email: "lol@lol.com"}})
+    }.to change(User,:count).by(1)
+  end
+
+  it "deberia encontrar un usuario si el uid y el provider ya existen" do
+    user = FactoryGirl.create(:user)
+    expect {
+      User.from_omniauth({uid: user.uid, provider: user.provider,info: {email: "lol@lol.com"}})
+    }.to change(User,:count).by(0)
+  end
+
+  it "deberia retornar el usuario si el uid y el provider ya existen" do
+    user = FactoryGirl.create(:user)
+    expect(
+      User.from_omniauth({uid: user.uid, provider: user.provider,info: {email: "lol@lol.com"}})
+      ).to eq(user)
+  end
 end
