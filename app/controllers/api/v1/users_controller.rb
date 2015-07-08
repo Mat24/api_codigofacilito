b02a0b37123bbe063ee5f437d10439202c84bf8c
Mat24 <smat_24@hotmail.com>
2015-07-08 17:18:58 -0500

    video: 2-9

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def create
     # params= { auth: { provider: 'facebook', uid: 'dawdadwd457'} }
-    @user = User.from_omniauth(params[:auth])
-    @token = @user.tokens.create
-    render "api/v1/users/show"
+    if !params[:auth]
+      render json: {error: "Auth param is missing"}, status: :unprocessable_entity
+    else
+      @user = User.from_omniauth(params[:auth])
+      @token = @user.tokens.create
+      render "api/v1/users/show"
+    end
   end
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+RSpec.describe Api::V1::UsersController,type: :request  do
+  describe "POST/users" do
+    before :each do
+      auth = {provider: "facebook", uid: "12dsadasd1y23g12", info: {email: "u@mail.com"} }
+      post "/api/v1/users", {auth: auth}
+    end
+
+    it { have_http_status(200) }
+
+    it {change(User,:count).by(1)}
+
+    it "responds with the user found or created" do
+      json = JSON.parse(response.body)
+      expect(json["email"]).to eq("u@mail.com")
+    end
+  end
+
+end
