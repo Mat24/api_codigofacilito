eb166524b573945a7463991d2741b871e9744b92
Mat24 <smat_24@hotmail.com>
2015-07-08 14:50:39 -0500

    video: 2-8

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::UsersController < ApplicationController
-  def index
+  def create
+    # params= { auth: { provider: 'facebook', uid: 'dawdadwd457'} }
+    @user = User.from_omniauth(params[:auth])
+    @token = @user.tokens.create
+    render "api/v1/users/show"
   end
 end
