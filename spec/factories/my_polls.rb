9625cf9b597bf1650641ccbebbd091bcab8dde72
Mat24 <smat_24@hotmail.com>
2015-07-10 08:56:59 -0500

    video: 3-1

---
+++ spec/factories/my_polls.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+
+  factory :my_poll do
+    user nil
+    expires_at "2015-07-09 10:35:42"
+    title "sadasdadadadasdadad"
+    description "asdasdadadsaadsdadasdasdasdasd"
+  end
+end
